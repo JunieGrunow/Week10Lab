6d05278138ecdfa4383ba5e08e5d6f19d95e5e91
Juniper <nuket@Juniper-HP-E>
2023-03-25 22:48:34 -0600

    Basic structure is down and the passwordUtil is built

---
+++ src/java/Fliters/AuthenticationFilter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fliters;
+
+import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.http.*;
+/**
+ *
+ * @author nuket
+ */
+public class AuthenticationFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
